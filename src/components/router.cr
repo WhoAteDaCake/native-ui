9b9d97e3b180f46ec31d0e9e8589bd79ae98affc
WhoAteDaCake <doitsimplefy@gmail.com>
2021-06-26 10:38:30 +0100

    feat(router): improved routing pages

--- src/components/router.cr
+++ src/components/router.cr
@@ -7,20 +7,21 @@ module Router
     property active : Page | Nil
     property history : Array(String)
     
-    delegate add, to: @tree
-
     def initialize(@root : Widget)
       @tree = Radix::Tree(Page).new
       @history = Array(String).new
     end
 
-    def change_page(route : String, page : Page, params : Hash(String, String), with_history = true)
+    def initialize(root : Widget, default_page : Page)
+      initialize(root)
+      @active = default_page
+      root.append_child(default_page.container)
+    end
+  
+    def change_page(route : String, page : Page, params : Hash(String, String))
       active = @active
       if active
         @root.unmount_child(active.container)
-      end
-      if with_history
-        @history << route
       end
       @active = page
       @root.append_child(page.container)
@@ -30,15 +31,25 @@ module Router
     def push(route : String)
       result = tree.find route
       if result.found?
-        change_page(route, result.payload, result.params, true)
+        change_page(route, result.payload, result.params)
+        @history << route
       end
     end
 
     def replace(route : String)
       result = tree.find route
       if result.found?
-        change_page(route, result.payload, result.params, false)
+        change_page(route, result.payload, result.params)
+        # Should never really happen, let's just validate to be safe
+        if @history.size > 0
+          @history[@history.size - 1] = route
+        end
       end
     end
+
+    def add(page : Page)
+      @tree.add(page.url, page)
+    end
+
   end
 end
